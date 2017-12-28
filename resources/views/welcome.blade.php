2b506e578d4235562aab9a2d9b11c700d11993e7
pedrada22 <pmcostaalcantara@gmail.com>
2017-12-28 02:54:01 -0200

    stilo1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,13 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
-        
+
         <script
         src="https://code.jquery.com/jquery-3.2.1.min.js"
         integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
         crossorigin="anonymous">
         </script>
-        
+
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -27,13 +27,12 @@
                 height: 100vh;
                 margin: 0;
             }
-            
+
             input[type=number] {
                  background-color: #fff;
                  color: #636b6f;
                  font-family: 'Raleway', sans-serif;
                  font-weight: 100;
-                 height: 100vh;
                  margin: 0;
              }
 
@@ -99,17 +98,17 @@
                 </div>
 
                 <div class="links">
-                    
+
                 </div>
-                
+
                 <table class="table table-bordered">
                     <tr><th>...</th><th>BTC</th><th>ETH</th><th>LTC</th><th>XRP</th><th>DASH</th><th>ZEC</th></tr>
                     <tr><td><div id="USD"><b>USD</b></div></td><td><div id="BTC"> 0 </div></td><td><div id="ETH"> 0 </div></td><td><div id="LTC"> 0 </div></td><td><div id="XRP"> 0 </div></td><td><div id="DASH"> 0 </div></td><td><div id="ZEC"> 0 </div></td></tr>
                     <tr><td><div id="Coins"><b>Coins</b></div></td><td><input type="number" id="coin-BTC" value="0"></td><td><input type="number"  id="coin-ETH" value="0">  </td><td><input type="number"  id="coin-LTC" value="0">  </td><td><input type="number"  id="coin-XRP" value="0">  </td><td><input type="number"  id="coin-DASH" value="0"> </td><td><input type="number"  id="coin-ZEC" value="0"> </td></tr>
                     <tr><td><div id="Total"><b>Total</b></div></td><td><div id="total-BTC">0</div></td><td><div id="total-ETH"> 0 </div></td><td><div id="total-LTC"> 0 </div></td><td><div id="total-XRP"> 0 </div></td><td><div id="total-DASH"> 0 </div></td><td><div id="total-ZEC"> 0 </div></td></tr>
-                    <tr><td><div id="Total_USD_desc"><b>Total est. USD</b></div></td><td><div id="total_USD_value">0</div></td><td><div id="desc_brl_usd"><b>U$D to BRL</b></div></td><td><div id="BRL_USD">0</div></td><td><div id="Total_BRL_desc"><b>Total est. BRL</b></div></td><td><div id="total_BRL_value">0</div></td>
+                    <tr><td><div id="Total_USD_desc"><b>Total USD</b></div></td><td><div id="total_USD_value">0</div></td><td><div id="desc_brl_usd"><b>U$D to BRL</b></div></td><td><div id="BRL_USD">0</div></td><td><div id="Total_BRL_desc"><b>Total est. BRL</b></div></td><td><div id="total_BRL_value">0</div></td>
                     <script source="">
-                    
+
                     var brl_usd_conv = 0;
                     $.getJSON( "http://api.promasters.net.br/cotacao/v1/valores?moedas=USD", function( data ) {
                         brl_usd_conv = parseFloat(data["valores"]["USD"]["valor"]);
@@ -117,19 +116,19 @@
                     });
                     var items = {};
                     $.getJSON( "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC,XRP,DASH,ZEC&tsyms=USD", function( data ) {
-                    
+
                         $.each( data, function( key, val ) {
                             $.each( val, function( key2, val2 ) {
                                 $("#"+key).html(val2);
                                 items[key]=val2;
                             });
                         });
-                    
-                    
+
+
                     });
-                    
-                    
-                    $( "[id^='coin-']").keyup(function() 
+
+
+                    $( "[id^='coin-']").keyup(function()
                     {
                         if ($(this).val()=="")
                         {
@@ -141,24 +140,24 @@
                             $("#total-"+x).html((parseFloat(items[x])*parseFloat($("#coin-"+x).val())).toFixed(2));
                         }
                         var total_usd=0;
-                        $("[id^='total-']").each(function( index ) 
+                        $("[id^='total-']").each(function( index )
                         {
                             total_usd += parseFloat($(this).text());
                         });
                         $("#total_USD_value").html(total_usd.toFixed(2));
                         $("#total_BRL_value").html((total_usd*brl_usd_conv).toFixed(2));
                     });
-                    
-                    
-                    
+
+
+
                     </script>
-                    
+
                 </table>
-                
+
             </div>
         </div>
-        
-        
-        
+
+
+
     </body>
 </html>
